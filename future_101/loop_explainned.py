a26b9c0800fa8fbdc5cf4899aefe10c12b7699d0
KikeVen <bruzual.enrique@outlook.com>
2019-05-09 10:54:21 -0400

    minor updates

--- future_101/loop_explainned.py
+++ future_101/loop_explainned.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# create an empty list, `shopping`, to hold the final list
+# create an empty list, `shopping`, to hold the final shopping list
 shopping = []
 
-# Assign an input request to the how_many variable
+# Assign an input request to the `how_many` variable
 how_many = input("how many items of shopping do you have? ")
 # change the input type to an interger by using the int() function
 # re-assign it to the same `how_many` variable
 how_many = int(how_many)
 
-# Using a for loop, iterate `how_many` times, and execute
-# the code with-in the loops scope 0 to `how_many` times
+# Using a for loop, execute the code with-in the
+# loops scope 0 to `how_many` times
 for item_number in range(how_many):
     # assign input to the `item` variable
     item = input("what is item number " + str(item_number) + "? ")
