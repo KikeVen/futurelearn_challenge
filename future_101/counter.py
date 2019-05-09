2883c0a53630ea2604af4d17aea92a603deaeca4
KikeVen <bruzual.enrique@outlook.com>
2019-05-08 20:04:17 -0400

    101_challenge

---
+++ future_101/counter.py
@@ -0,0 +1,9 @@
+
+shopping = ["bread", "cheese", "apple", "tomato", "biscuits"]
+count = 0
+
+for item in shopping:
+    print(item)
+    count = count + 1
+
+print('You have', count, 'items in your shopping list')
